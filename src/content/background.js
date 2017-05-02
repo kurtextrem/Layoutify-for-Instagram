bf2e206517f367a8580e824f816e6d8020299d96
Jacob Groß <kurtextrem@gmail.com>
2017-05-02 17:46:15 +0200

    Fix createTab when tab got closed

--- src/content/background.js
+++ src/content/background.js
@@ -1,9 +1,12 @@
 'use strict'
 
 var tab = null, url = chrome.runtime.getURL('index.html')
-function createTab(id) {
-	if (tab !== null) {
-		chrome.tabs.update({ id: tab.id }, { active: true, url: url + '?tabid=' + id })
+function createTab(id, force) {
+	if (tab !== null && !force) {
+		chrome.tabs.update(tab.id, { active: true, url: url + '?tabid=' + id }, function() {
+			if (chrome.runtime.lastError)
+				createTab(id, true)
+		})
 	} else {
 		chrome.tabs.create({
 			url: url + '?tabid=' + id
@@ -16,6 +19,6 @@ function createTab(id) {
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
 		if (request.action === 'click')
-			createTab(sender.tab.id)
+			createTab(sender.tab.id, false)
 	}
 )
