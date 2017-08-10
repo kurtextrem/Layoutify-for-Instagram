7b8e1424fdcf96c7041d38d901798d2cf1fa0fd1
Jacob Groß <kurtextrem@gmail.com>
2017-08-10 16:02:05 +0200

    Try to reduce background memory usage

--- src/content/background.js
+++ src/content/background.js
@@ -1,73 +1,78 @@
 'use strict'
 
-const url = chrome.runtime.getURL('index.html')
-let tab = null
+const API = 'https://www.instagram.com/'
+const UA = 'Instagram 10.8.0 Android (24/7.0; 380dpi; 1080x1920; OnePlus; ONEPLUS A3010; OnePlus3T; qcom; en_US)'
 
+const url = chrome.runtime.getURL('index.html')
+
+let tab = null
+function create(newTab) {
+	tab = newTab
+}
+
+function update() {
+	if (chrome.runtime.lastError) createTab(this, true)
+}
+
+const updateObj = { active: true, url: '' }
+const createObj = { url: '' }
 function createTab(id, force) {
 	if (tab !== null && !force) {
-		chrome.tabs.update(tab.id, { active: true, url: url + '?tabid=' + id }, function update() {
-			if (chrome.runtime.lastError)
-				createTab(id, true)
-		})
+		updateObj.url = url + '?tabid=' + id
+		chrome.tabs.update(tab.id, updateObj, update.bind(id))
 	} else {
-		chrome.tabs.create({
-			url: url + '?tabid=' + id
-		}, function create(newTab) {
-			tab = newTab
-		})
+		createObj.url = url + '?tabid=' + id
+		chrome.tabs.create(createObj, create)
 	}
 }
 
-chrome.runtime.onMessage.addListener(
-	function listener(request, sender, sendResponse) {
-		if (request.action === 'click')
-			createTab(sender.tab.id, false)
-	}
-)
+chrome.runtime.onMessage.addListener(function listener(request, sender, sendResponse) {
+	if (request.action === 'click') createTab(sender.tab.id, false)
+})
 
-var API = 'https://www.instagram.com/'
+const cookieObj = { url: API, name: '' }
 function getCookie(name) {
 	return new Promise((resolve, reject) => {
-		chrome.cookies.get({ url: API, name }, function cookies(cookie) {
+		cookieObj.name = name
+		chrome.cookies.get(cookieObj, function cookies(cookie) {
 			if (cookie !== null) resolve(cookie.value)
 			reject()
 		})
 	})
 }
 
-var sessionid = ''
+let sessionid = ''
 function getSessionId() {
-	getCookie('sessionid').then(function saveSession(value) {
-		return sessionid = value
-	})
+	getCookie('sessionid').then(saveSession)
 }
+function saveSession(value) {
+	return (sessionid = value)
+}
+
 getSessionId()
 
 // hook into web request and modify headers before sending the request
-chrome.webRequest.onBeforeSendHeaders.addListener(function listener(details) {
-	getSessionId() // just update for next time
+chrome.webRequest.onBeforeSendHeaders.addListener(
+	function listener(details) {
+		getSessionId() // just update for next time
 
-	const headers = details.requestHeaders
+		const headers = details.requestHeaders
 
-	for (let i = 0; i < headers.length; ++i) {
-		const header = headers[i]
+		for (let i = 0; i < headers.length; ++i) {
+			var header = headers[i]
 
-		if (header.name === 'User-Agent') {
-			header.value = 'Instagram 10.8.0 Android (24/7.0; 380dpi; 1080x1920; OnePlus; ONEPLUS A3010; OnePlus3T; qcom; en_US)'
-		} else if (header.name === 'Cookie') {
-			// add auth cookies to authenticate API requests
-			const cookies = header.value + '; sessionid=' + sessionid
-			header.value = cookies
+			if (header.name === 'User-Agent') {
+				header.value = UA
+			} else if (header.name === 'Cookie') {
+				// add auth cookies to authenticate API requests
+				header.value = header.value + '; sessionid=' + sessionid
+			}
 		}
-	}
-	return { requestHeaders: headers }
-},
+		return { requestHeaders: headers }
+	},
 	{
-		urls: [
-			'https://i.instagram.com/api/v1/feed/liked/*',
-			'https://i.instagram.com/api/v1/feed/saved/*'
-		],
-		types: ['xmlhttprequest']
+		urls: ['https://i.instagram.com/api/v1/feed/liked/*', 'https://i.instagram.com/api/v1/feed/saved/*'],
+		types: ['xmlhttprequest'],
 	},
 	['blocking', 'requestHeaders']
 )
