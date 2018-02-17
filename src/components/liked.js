10c5e4870367fee2da100dc7a3c5a8d097c4c09c
Jacob Groß <kurtextrem@gmail.com>
2018-02-17 17:55:15 +0100

    Make Liked/Saved alias instead of stateless components

--- src/components/liked.js
+++ src/components/liked.js
@@ -1,5 +1,4 @@
 import PostsContainer from './PostsContainer'
 import { createElement } from 'nervjs'
 
-const Liked = () => <PostsContainer id="liked" defaultClass="favorite" toggleClass="favorite_border" />
-export default Liked
+export default <PostsContainer id="liked" defaultClass="favorite" toggleClass="favorite_border" />
--- src/components/saved.js
+++ src/components/saved.js
@@ -1,5 +1,4 @@
 import PostsContainer from './PostsContainer'
 import { createElement } from 'nervjs'
 
-const Saved = () => <PostsContainer id="saved" defaultClass="turned_in" toggleClass="turned_in_not" />
-export default Saved
+export default <PostsContainer id="saved" defaultClass="turned_in" toggleClass="turned_in_not" />
