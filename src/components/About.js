9d47ae0fe35288588bcd55cbc3455253ee74d38c
Jacob Groß <kurtextrem@gmail.com>
2020-04-09 10:13:09 +0200

    update about changelog

--- src/components/About.js
+++ src/components/About.js
@@ -20,26 +20,18 @@ export default class About extends Component {
 			<div>
 				<h3>About</h3>
 				<p>
-					Improved Layout for Instagram does not store any data of your
-					Instagram account apart from on your PC. The Code is Open Source on{' '}
-					<a
-						href="https://github.com/kurtextrem/Improved-for-Instagram"
-						target="_blank"
-						rel="noopener">
+					Improved Layout for Instagram does not store any data of your Instagram account apart from on your PC. The Code is Open Source on{' '}
+					<a href="https://github.com/kurtextrem/Improved-for-Instagram" target="_blank" rel="noopener">
 						GitHub
 					</a>
 					.
 				</p>
 				<p>
 					If you find any issues, please report any bugs on{' '}
-					<a
-						href="https://github.com/kurtextrem/Improved-for-Instagram/issues"
-						target="_blank"
-						rel="noopener">
+					<a href="https://github.com/kurtextrem/Improved-for-Instagram/issues" target="_blank" rel="noopener">
 						GitHub
 					</a>{' '}
-					or mail me:{' '}
-					<a href="mailto:kurtextrem@gmail.com">kurtextrem@gmail.com</a>.
+					or mail me: <a href="mailto:kurtextrem@gmail.com">kurtextrem@gmail.com</a>.
 				</p>
 				<p>
 					I'd also very much appreciate <span class="star" />
@@ -56,14 +48,13 @@ export default class About extends Component {
 					.
 				</p>
 				<p>
-					Follow me on <a href="https://instagram.com/kurtextrem">Instagram</a>{' '}
-					or <a href="https://twitter.com/kurtextrem">Twitter</a> for updates!
+					Follow me on <a href="https://instagram.com/kurtextrem">Instagram</a> or <a href="https://twitter.com/kurtextrem">Twitter</a> for
+					updates!
 				</p>
 
 				<h3>Clear Outdated Data</h3>
 				<p>
-					Sometimes old posts are displayed, which aren't saved or liked
-					anymore. Use this button to clear old data (not options):{' '}
+					Sometimes old posts are displayed, which aren't saved or liked anymore. Use this button to clear old data (not options):{' '}
 					<Button color="warning" onClick={this.onBtnClick}>
 						Clear
 					</Button>
@@ -72,10 +63,7 @@ export default class About extends Component {
 				<h3>Tips & Tricks</h3>
 				<p>
 					If you want to upload pictures to Instagram, follow{' '}
-					<a
-						href="https://medium.com/@mwender/how-to-post-to-instagram-from-your-desktop-65fa55c77556"
-						target="_blank"
-						rel="noopener">
+					<a href="https://medium.com/@mwender/how-to-post-to-instagram-from-your-desktop-65fa55c77556" target="_blank" rel="noopener">
 						this
 					</a>{' '}
 					tutorial.
@@ -99,12 +87,11 @@ export default class About extends Component {
 					</b>{' '}
 					for the modern and nice new logo.
 					<br />
-					And to all the bug reporters that have sent me mails. I always try my
-					best to respond as soon as possible.
+					And to all the bug reporters that have sent me mails. I always try my best to respond as soon as possible.
 				</p>
 
 				<h3>
-					<a href="#/changelog">
+					<a href="https://github.com/kurtextrem/Improved-for-Instagram/blob/master/CHANGELOG.md#changelog" target="_blank" rel="noopener">
 						Changelog <i class="material-icons">description</i>
 					</a>
 				</h3>
@@ -112,18 +99,13 @@ export default class About extends Component {
 				<small>
 					<b>Legal</b>
 					<br />
-					This project is in no way affiliated with, authorized, maintained,
-					sponsored or endorsed by Instagram or any of its affiliates or
+					This project is in no way affiliated with, authorized, maintained, sponsored or endorsed by Instagram or any of its affiliates or
 					subsidiaries. This is an independent project. Use at your own risk.
 					<br />
-					<a
-						href="https://kurtextrem.de/chrome/PRIVACY.html"
-						target="_blank"
-						rel="noopener">
+					<a href="https://kurtextrem.de/chrome/PRIVACY.html" target="_blank" rel="noopener">
 						Privacy Policy
 					</a>{' '}
-					(Sensitive data is <b>only</b> collected and stored{' '}
-					<b>on your own PC</b>, options are synced if Chrome Sync is turned on)
+					(Sensitive data is <b>only</b> collected and stored <b>on your own PC</b>, options are synced if Chrome Sync is turned on)
 				</small>
 			</div>
 		)
