441fe9819876af95afc7bb5a55445d7e4f867b7c
Jacob Groß <kurtextrem@gmail.com>
2021-06-17 18:23:32 +0200

    skip injecting on embeds

--- src/content/start.js
+++ src/content/start.js
@@ -1,24 +1,23 @@
 /**
  * Chrome Extensions don't have access to `window` props when not being in the page scope.
  * So we inject the main functions into the page in order to get access.
- *
- * @author 	Jacob Groß
- * @date   	2016-03-01
  */
-;(function inject(document) {
+;(function inject() {
 	'use strict'
+
+	if (location.pathname.indexOf('/embed/') > 0) return // do not do stuff on embeds, > 0, because a user could be called embed.
 
 	const scripts = ['runtime.bundle.js', 'commons.bundle.js', 'feed.bundle.js', 'content/igdata.js']
 
-	scripts.forEach(v => {
+	for (let i = 0; i < scripts.length; ++i) {
 		const s = document.createElement('script')
-		s.src = chrome.extension.getURL(v)
+		s.src = chrome.extension.getURL(scripts[i])
 		document.body.appendChild(s)
-	})
+	}
 
 	const c = document.createElement('link')
 	c.href = chrome.extension.getURL('feed.css')
 	c.rel = 'stylesheet'
 	c.id = 'ige_feedCSS'
 	document.body.appendChild(c)
-})(window.document)
+})()
